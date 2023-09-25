c53936c1b696aa37e45f4814bb465906d76021da
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2023-09-25 16:13:43 +0300

    Изменения в окне брони

--- app/src/main/java/com/example/myroom/ReservationActivity.kt
+++ app/src/main/java/com/example/myroom/ReservationActivity.kt
@@ -2,10 +2,17 @@ package com.example.myroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TimePicker
 
 class ReservationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation)
+
+        val startTimePicker = findViewById<TimePicker>(R.id.startTimePicker)
+        val endTimePicker = findViewById<TimePicker>(R.id.endTimePicker)
+
+        startTimePicker.setIs24HourView(true)
+        endTimePicker.setIs24HourView(true)
     }
 }
